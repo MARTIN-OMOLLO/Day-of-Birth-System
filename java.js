5351b009b1bd361039a014433b4430c0af3c37be
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2021-12-12 21:07:55 +0300

    lincenced

--- java.js
+++ java.js
@@ -1,11 +1,13 @@
-var date = document.getElementById("Date").value
+
+document.getElementById("button").addEventListener("click", function() {
+    console.log("java")
+    var date = document.getElementById("Date").value
 var month = document.getElementById("Month").value
 var year = document.getElementById("Year").value
 var form = document.querySelector(".two")
-
-document.getElementById("submit").addEventListener("sumbit", function(e) {
 var dateEntered = new Date(year, month, date);
 var dayOfWeek = dateEntered.getDay();
 console.log(dayOfWeek)
+document
 
 });
